761e88bf989313328701e2f35b08391f2bcab92d
dlrlgugu <hckguja@gmail.com>
2018-05-29 16:03:30 -0700

    added def optimize Signed-off-by: dlrlgugu <hckguja@gmail.com>

--- image recognition.py
+++ image recognition.py
@@ -81,3 +81,39 @@ def propagate(w, b, X, Y):
     
     return grads, cost
 
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    costs = []
+    
+    for i in range(num_iterations):
+        
+        
+        # Cost and gradient calculation (≈ 1-4 lines of code)
+        ### START CODE HERE ### 
+        grads, cost = propagate(w,b,X,Y)
+        ### END CODE HERE ###
+        
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        # update rule (≈ 2 lines of code)
+        ### START CODE HERE ###
+        w = w-learning_rate*dw
+        b = b-learning_rate*db
+        ### END CODE HERE ###
+        
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        # Print the cost every 100 training iterations
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
