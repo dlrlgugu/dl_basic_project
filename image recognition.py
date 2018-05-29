81d5e926b94296d8ef9bcdcd6736f559900a8c59
dlrlgugu <hckguja@gmail.com>
2018-05-29 14:38:46 -0700

    fixed utils.py and works first version. Signed-off-by: dlrlgugu <hckguja@gmail.com>

--- image recognition.py
+++ image recognition.py
@@ -7,3 +7,10 @@ from scipy import ndimage
 from utils import load_dataset
 
 
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+index = 25
+plt.imshow(train_set_x_orig[index])
+plt.show()
+print(str(train_set_y[:,index]) +
+      "it's a "+classes[np.squeeze(train_set_y[:,index])].decode("utf-8") )
