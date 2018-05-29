b3fdcdfd6052cfd463a6e2c9570fbd3e4575d1ec
dlrlgugu <hckguja@gmail.com>
2018-05-29 16:38:41 -0700

    First model. Signed-off-by: dlrlgugu <hckguja@gmail.com>

--- image recognition.py
+++ image recognition.py
@@ -67,7 +67,7 @@ def propagate(w, b, X, Y):
     
     # BACKWARD PROPAGATION (TO FIND GRAD)
     ### START CODE HERE ### (≈ 2 lines of code)
-    dw = (1/m)*np.dot(X,(A - Y).T))
+    dw = (1/m)*np.dot(X,(A - Y).T)
     db = 1/m*np.sum(A-Y)
     ### END CODE HERE ###
 
@@ -117,3 +117,70 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
              "db": db}
     
     return params, grads, costs
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+
+    A=sigmoid(np.dot(w.T,X)+b)
+
+    for i in range(A.shape[1]):
+        if A[0,i] > 0.5:
+            Y_prediction[0,i] = 1
+        else :
+            Y_prediction[0,i] = 0
+
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
+    
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    # Gradient descent (≈ 1 line of code)
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    # Predict test/train set examples (≈ 2 lines of code)
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    ### END CODE HERE ###
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+
+index = 5
+plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
+plt.show()
+
+#print ("y = " + (test_set_y[0, index]))
+#print (classes[d["Y_prediction_test"][0, index]].decode("utf-8") )
+
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('costs')
+plt.xlabel('iterations')
+plt.title("learning rate ="+str(d["learning_rate"]))
+plt.show()
+
