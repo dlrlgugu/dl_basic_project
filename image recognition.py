2ffc381f7fc654be119e44045cb81dabe30e60f3
dlrlgugu <hckguja@gmail.com>
2018-05-29 15:50:44 -0700

    initialize parameters Signed-off-by: dlrlgugu <hckguja@gmail.com>

--- image recognition.py
+++ image recognition.py
@@ -22,6 +22,37 @@ print(classes.shape)#(2,)
 
 print(train_set_x_orig.shape[0])
 
-#m_train=
-#m_test=
-#num_px=
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],-1).T
+
+print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+print ("train_set_y shape: " + str(train_set_y.shape))
+print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+print ("test_set_y shape: " + str(test_set_y.shape))
+print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
+
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+
+#dimensions and shapes of problem
+#reshape
+#standardize.
+
+def sigmoid(z):
+    s = 1/(1+np.exp(-z))
+    return s
+
+
+def initialize_with_zeros(dim):
+    w = np.zeros((dim,1))
+    b = 0
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
+
